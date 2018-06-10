2c5643388ecd205ab2b998a8ab9e7f56e17a8c03
AndryK <a.kriganovsky@gmail.com>
2018-06-10 15:44:15 +0300

    Completed generation short urls

--- api/controllers/urlController.js
+++ api/controllers/urlController.js
@@ -6,23 +6,22 @@ var mongoose = require('mongoose'),
 //#region User
 exports.create_a_url = async function (req, res) {
     //valid short_url
-    var host_url = "lockalhost:4000";
-    var  url  = req.body.url;
+    var host_url = "localhost:4000";
+    var url = req.body.url;
     if (validUrl.isUri(url)) {
         req.body.url_code = makeSortUrl(6);
         req.body.short_url = host_url + "/" + req.body.url_code;
         var new_url = new Url(req.body);
         new_url.save(function (err, url) {
             if (err) { res.send(err); }
-
-            res.json(url);
+                res.json(url);
         });
     }
     else {
         return res
             .status(404)
             .json(
-                "Invalid Url"
+                {message: "Invalid Url"}
             );
     };
 
@@ -36,10 +35,14 @@ exports.list_all_urls = function (req, res) {
 };
 
 exports.redirect =async function (req, res) {
-    var url_code1 = req.body.url_code;
-   await Url.findOne({ url_code: url_code1 }, function (err, url) {
+    var url_code = req.param('url_code');
+    Url.findOne({'url_code': url_code }, await function (err, url) {
         if (err) { res.send(err); }
-       res.redirect(url);
+        if (url != null) {            
+            return res.redirect(url.url);
+        } else {
+            res.send("Short url not found");
+        }        
     });
 };
 //coding function url
