17a9b9039704178b03d0b19b38257f72968035da
AndryK <a.kriganovsky@gmail.com>
2018-06-11 12:16:49 +0300

    Added validated user short url

--- api/controllers/urlController.js
+++ api/controllers/urlController.js
@@ -3,39 +3,64 @@ require('../models/urlModel');
 var mongoose = require('mongoose'),
     validUrl = require("valid-url"),
     Url = mongoose.model('Urls');
-//#region User
+//create url
 exports.create_a_url = async function (req, res) {
     //valid short_url
     var host_url = "localhost:4000";
     var url = req.body.url;
-    if (validUrl.isUri(url)) {
-        req.body.url_code = makeSortUrl(6);
-        req.body.short_url = host_url + "/" + req.body.url_code;
-        var new_url = new Url(req.body);
-        new_url.save(function (err, url) {
-            if (err) { res.send(err); }
-                res.json(url);
-        });
+    var userUrl=req.body.userShortUrl;
+    var record = await Url.findOne({'url_code':userUrl});
+    if (userUrl) {
+        if (record!=null) {
+            res.status(404).json({message: "Invalid short Url"});
+        } else {
+            if (validUrl.isUri(url)) {
+                req.body.url_code = userUrl;
+                req.body.short_url = host_url + "/" + req.body.url_code;
+                var new_url = new Url(req.body);
+                new_url.save(function (err, url) {
+                    if (err) { res.send(err); }
+                        res.json(url);
+                });
+            }
+            else {
+                return res
+                    .status(404)
+                    .json(
+                        {message: "Invalid Url"}
+                    );
+            }; 
+        }
     }
-    else {
-        return res
-            .status(404)
-            .json(
-                {message: "Invalid Url"}
-            );
-    };
-
+    else{
+        if (validUrl.isUri(url)) {
+            req.body.url_code = makeSortUrl(6);
+            req.body.short_url = host_url + "/" + req.body.url_code;
+            var new_url = new Url(req.body);
+            new_url.save(function (err, url) {
+                if (err) { res.send(err); }
+                    res.json(url);
+            });
+        }
+        else {
+            return res
+                .status(404)
+                .json(
+                    {message: "Invalid Url"}
+                );
+        };
+    }
 };
-
+//list all users
 exports.list_all_urls = function (req, res) {
     Url.find({}, function (err, url) {
         if (err) { res.send(err); }
         res.json(url);
     });
 };
-
+//redirect this shot url
 exports.redirect =async function (req, res) {
-    var url_code = req.param('url_code');
+    var url_code = req.params.url_code;
     Url.findOne({'url_code': url_code }, await function (err, url) {
         if (err) { res.send(err); }
         if (url != null) {            
